3d8f595a43d8488afbaca4bdef2532179c323d55
Aliaksandr Shtarou <aeresfiru@gmail.com>
2023-04-13 20:17:07 +0300

    Adding validation, mockk kotlin and jackson dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -19,10 +20,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    //implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.h2database:h2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
