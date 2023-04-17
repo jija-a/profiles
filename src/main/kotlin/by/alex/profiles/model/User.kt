7608d8d58a998b640b6b44b9a2e9964006c38ae1
Aliaksandr Shtarou <aeresfiru@gmail.com>
2023-04-17 14:03:48 +0300

    Adding JPA to user class

--- src/main/kotlin/by/alex/profiles/model/User.kt
+++ src/main/kotlin/by/alex/profiles/model/User.kt
@@ -1,14 +1,23 @@
 package by.alex.profiles.model
 
-import java.io.Serializable
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
 import java.time.LocalDateTime
 
+@Entity
 data class User(
-    val id: Long? = null,
-    var firstName: String,
-    var lastName: String,
-    var email: String,
-    var password: String,
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+    var firstName: String = "",
+    var lastName: String = "",
+    var email: String = "",
+    var password: String = "",
     var registrationDate: LocalDateTime = LocalDateTime.now(),
     var role: Role = Role.USER
-) : Serializable
+) {
+    constructor() : this(null, "", "", "", "", LocalDateTime.now(), Role.USER)
+}
