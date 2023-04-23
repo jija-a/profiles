2bb148002676d0d49531719c1319e7cc7320320a
Aliaksandr Shtarou <aeresfiru@gmail.com>
2023-04-23 20:36:29 +0300

    fixup! Adding methodargtypemismatch handling and swagger to globalexchandler

--- src/main/kotlin/by/alex/profiles/exception/ErrorMessages.kt
+++ src/main/kotlin/by/alex/profiles/exception/ErrorMessages.kt
@@ -6,4 +6,5 @@ object ErrorMessages {
     const val REQUEST_BODY_EMPTY = "error.message.emptyRequestBody"
     const val NO_HANDLER_FOUND = "error.message.noHandlerFound"
     const val INTERNAL = "error.message.internal"
+    const val METHOD_ARG_TYPE_MISMATCH= "error.message.methodArgTypeMismatch"
 }
